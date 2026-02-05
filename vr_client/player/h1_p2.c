1a71e18d30eb55fcf7eb53c49e99925116e198ea
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2026-02-05 11:03:07 -0300

    Fix header order in the session data files

--- vr_client/player/h1_p2.c
+++ vr_client/player/h1_p2.c
@@ -742,7 +742,7 @@ int main(int argc, char **argv)
     strcat(filename2, "\0");
     // filename logfile
     flog2 = fopen(filename2, "wb");
-    char session_metrics[187] = "til_720_z1, til_1080_z1, til_4k_z1, til_720_z2, til_1080_z2, til_4k_z2, til_720_z3, til_1080_z3, z1_bit, z2_bit, z3_bit, til_4k_z3, qt_sw_z1, qt_sw_z2, qt_sw_z3, total_stall, start_time\n"
+    char session_metrics[187] = "til_720_z1, til_1080_z1, til_4k_z1, til_720_z2, til_1080_z2, til_4k_z2, til_720_z3, til_1080_z3, til_4k_z3, z1_bit, z2_bit, z3_bit, qt_sw_z1, qt_sw_z2, qt_sw_z3, total_stall, start_time\n"
     fprintf(flog2, session_metrics);
     ///////////////////////
 
@@ -1709,6 +1709,8 @@ int main(int argc, char **argv)
         }
 
         gettimeofday(&end, NULL);
+
+        "til_720_z1, til_1080_z1, til_4k_z1, til_720_z2, til_1080_z2, til_4k_z2, til_720_z3, til_1080_z3, til_4k_z3, z1_bit, z2_bit, z3_bit, qt_sw_z1, qt_sw_z2, qt_sw_z3, total_stall, start_time\n"
 
         fprintf(flog2, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %.6lf, %.6lf, %.6lf, %d, %d, %d, %.6lf, %.6lf\n", cont720z1, cont1080z1, cont4kz1, cont720z2, cont1080z2, cont4kz2, cont720z3, cont1080z3, cont4kz3, ((double)avgbitratez1 / (double)(contz1 + stall_count_vp)), ((double)avgbitratez2 / (double)(contz2 + stall_count_adj)), ((double)avgbitratez3 / (double)(contz3 + stall_count_out)), switchVp, switchAdj, switchOut, stall_len, startup_time);
 
