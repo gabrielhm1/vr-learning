9ca478e39335962e3d9756450f6a9ba0e0937555
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2026-03-04 18:30:52 -0300

    Bound stall penalty and remove unused functions

--- gym_hpa/envs/util.py
+++ gym_hpa/envs/util.py
@@ -1,57 +1,18 @@
 import csv
-
-
-def save_obs_to_csv(file_name, timestamp, num_pods, desired_replicas, cpu_usage, mem_usage,
-                    traffic_in, traffic_out, latency, lstm_1_step, lstm_5_step):
-    file = open(file_name, 'a+', newline='')  # append
-    # file = open(file_name, 'w', newline='') # new
-    with file:
-        fields = ['date', 'num_pods', 'cpu', 'mem', 'desired_replicas',
-                  'traffic_in', 'traffic_out', 'latency', 'lstm_1_step', 'lstm_5_step']
-        writer = csv.DictWriter(file, fieldnames=fields)
-        # writer.writeheader() # write header
-        writer.writerow(
-            {'date': timestamp,
-             'num_pods': int("{}".format(num_pods)),
-             'cpu': int("{}".format(cpu_usage)),
-             'mem': int("{}".format(mem_usage)),
-             'desired_replicas': int("{}".format(desired_replicas)),
-             'traffic_in': int("{}".format(traffic_in)),
-             'traffic_out': int("{}".format(traffic_out)),
-             'latency': float("{:.3f}".format(latency)),
-             'lstm_1_step': int("{}".format(lstm_1_step)),
-             'lstm_5_step': int("{}".format(lstm_5_step))}
-        )
-
-
-def save_to_csv(file_name, episode, avg_pods, avg_latency, reward, execution_time):
-    file = open(file_name, 'a+', newline='')  # append
-    # file = open(file_name, 'w', newline='')
-    with file:
-        fields = ['episode', 'avg_pods', 'avg_latency', 'reward', 'execution_time']
-        writer = csv.DictWriter(file, fieldnames=fields)
-        # writer.writeheader()
-        writer.writerow(
-            {'episode': episode,
-             'avg_pods': float("{:.2f}".format(avg_pods)),
-             'avg_latency': float("{:.4f}".format(avg_latency)),
-             'reward': float("{:.2f}".format(reward)),
-             'execution_time': float("{:.2f}".format(execution_time))}
-        )
-
+import math
 
 def get_stall_penalty(deployment_list):
     # Scaling factors
-    scale_duration = 10.0  # seconds
-    scale_count = 5.0      # number of stalls
+    scale_duration = 20.0  # seconds
+    scale_count = 10.0      # number of stalls
 
-    # Extract pod metrics
+    # Extract client metrics
     for d in deployment_list:
         stall_duration = d.stall_duration
         stall_count = d.stall_count
 
-    # Calculate soft-scaled penalty (unbounded, continuous)
-    p_qoe = (stall_duration / scale_duration) + (stall_count / scale_count)
+    # Calculate bounded penalty
+    p_qoe = math.tanh((stall_duration / scale_duration) + (stall_count / scale_count))
 
     return p_qoe
 
@@ -75,7 +36,7 @@ def get_qoe_reward(deployment_list):
 
     p_stall = get_stall_penalty(deployment_list)
     p_cost = get_cost_penalty(deployment_list)
-    reward = - ((alpha * p_cost) + (beta * p_stall))
+    reward = - ((alpha * p_stall) + (beta * p_cost))
 
     return reward
 
